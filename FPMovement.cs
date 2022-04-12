91d7fffa8f668d491dd343780153fbd64a2486c0
SoapyTarantula <soapytarantula@gmail.com>
2022-04-12 13:42:07 -0500

    Normalization of syntax
    also added comments & made some stuff clearer. plus spelling errors lol

--- FPMovement.cs
+++ FPMovement.cs
@@ -3,55 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Uses a Character Controller component to handle colliders and stuff.
-// This is extremely basic and could be down in many different, fancier ways.
+// This is also using the old input system.
+// This is extremely basic and could be done in many different, fancier ways.
 // I think this was heavily based on a Brackey's controller, can't remember.
 
 public class FPMovement : MonoBehaviour
 {
-    public CharacterController controller;
-    public Transform groundCheck;
-    public float groundDist = 0.25f;
-    public LayerMask groundMask;
-    public float gravityVelocity = -9.8f * 2;
-    Vector3 velocity;
+    [SerializeField] CharacterController _controller;
+    [SerializeField] Transform _groundCheck; // Uses an empty with checksphere to determine if we're grounded.
+    [SerializeField] float _groundDist = 0.25f; // The radius of the above checksphere.
+    [SerializeField] LayerMask _groundMask; // Used to figure out what is the ground
+    [SerializeField] float _playerLowerVerticalBounds = -5f; // Used as a lower y axis limit for the player in the IsPlayerFucked() method. Default is -5f
+
+    public float gravityVelocity = -9.8f * 2; // Gravity. Public instead of private in case we want to have some things affect gravity for the player.
     public float speed = 8f; // Move speed
     public float jumpVelocity = 6f; // Jumping velocity to add
-    private bool isGrounded;
+
+    private Vector3 _velocity; // Variable to store our velocity and shit.
+    private bool _isGrounded;
+
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask); // are we on the floor?
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDist, _groundMask); // are we on the floor?
 
-        if(isGrounded && velocity.y < 0) // prevents floaty anti-gravity shit
+        if(_isGrounded && _velocity.y < 0) // prevents floaty anti-gravity shit
         {
-            velocity.y = -2f;
+            _velocity.y = -2f;
         }
-
+        // Old style input axes.
         float xAxis = Input.GetAxisRaw("Horizontal");
         float zAxis = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = transform.right * xAxis + transform.forward * zAxis; // get player movement input
+        Vector3 move = transform.right * xAxis + transform.forward * zAxis; // get player movement input and normalize it below.
         move.Normalize();
 
-        controller.Move(speed * Time.deltaTime * move); // apply movement to player entity
+        _controller.Move(speed * Time.deltaTime * move); // apply movement to player entity
 
-        if(Input.GetButtonDown("Jump") && isGrounded) // checking to see if we can jump, and jumping if so.
+        if(Input.GetButtonDown("Jump") && _isGrounded) // checking to see if we can jump, and jumping if so.
         {
-            velocity.y = jumpVelocity; 
+            _velocity.y = jumpVelocity; 
         }
 
-        velocity.y -= gravityVelocity * Time.deltaTime; // applying gravity to player
+        _velocity.y -= gravityVelocity * Time.deltaTime; // applying gravity to player
 
-        controller.Move(velocity * Time.deltaTime); // more gravity shit
-        IsPlayerFucked();
+        _controller.Move(_velocity * Time.deltaTime); // more gravity shit
+        IsPlayerFucked(); // Did the player fall out of the world?
     }
 
     // This will put the player back into the play area if they somehow fall out of the world.
     void IsPlayerFucked()
     {
-        if (controller.transform.position.y <= -5f)
+        if (_controller.transform.position.y <= -_playerLowerVerticalBounds)
         {
             print("The player fell out of the world somehow");
-            controller.transform.position = new Vector3(0, 2, 0);
+            _controller.transform.position = new Vector3(0, 2, 0); // Set this vector 3 to somewhere in your playable area.
         }
     }
 }
